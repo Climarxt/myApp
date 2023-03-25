6054902f0aaada4a4befb9b35c5bf29895cba77e
Climarxt <christianbastide95@gmail.com>
2023-03-25 16:33:25 +0100

    Revert "Rename screens"
    This reverts commit 0816f5532de1bd5fc4b1a795b645db9fa3211515.

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
 
-import 'screens/screens.dart';
+import 'app/apps.dart';
 import 'pages/pages.dart';
 
 class MainPage extends StatefulWidget {
@@ -48,7 +48,6 @@ class _MainPageState extends State<MainPage> {
           selectedIndex: _selectedPageIndex,
           onDestinationSelected: _onDestinationSelected,
           labelType: NavigationRailLabelType.selected,
-          // Logo
           leading: Column(
             children: [
               // const SizedBox(height: 8),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/widgets/customcard.dart';
 
-import '../screens/screens.dart';
+import '../app/apps.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
